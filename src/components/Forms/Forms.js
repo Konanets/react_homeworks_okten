d240a1ffd42a78138219233a32ab8ec47223aa5f
iwan <ivankonanezs4@gmail.com>
2022-09-20 21:36:46 +0200

    init

---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Forms = () => {
+    return (
+           <form>
+               <h1>User Form</h1>
+                <div>
+                    <label htmlFor="user-name">privet
+                        <input type="text"/>
+                    </label>
+                </div>
+           </form>
+    );
+};
+
+export default Forms;
