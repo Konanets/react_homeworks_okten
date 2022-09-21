4c49ea70dc5fdca65ee687a7c07624a0e6dafba7
iwan <ivankonanezs4@gmail.com>
2022-09-21 18:41:44 +0200

    init

--- src/validators/CarFormValidator.js
+++ src/validators/CarFormValidator.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const carFormValidator=Joi.object({
-    model:Joi.string().pattern(/\w{0,15}/).required(),
+    model:Joi.string().required(),
     price:Joi.number().min(0).max(5000000).required(),
     year:Joi.number().min(1999).max(new Date().getFullYear()).required()
 });
