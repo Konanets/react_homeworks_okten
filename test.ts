231baa0ef0be4a3ebc350650031d83fb6e6dfc55
iwan <ivankonanezs4@gmail.com>
2022-10-12 14:21:42 +0200

    init

--- test.ts
+++ test.ts
@@ -39,39 +39,37 @@
 // }
 
 
-export interface ISpaceX<T> {
-    mission_name: string,
-    launch_date_local: string,
+interface ICore {
+    flight: number;
+    core: {
+        reuse_count: number;
+        status: unknown | boolean;
+    };
+}
+
+interface IPayloads {
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number;
+}
+
+export interface ISpaceX {
+    mission_name: string;
+    launch_date_local: Date;
     launch_site: {
-        site_name_long: string
-    },
+        site_name_long: string;
+    };
     links: {
-        article_link: T,
-        video_link: string
-    },
+        article_link: null | string;
+        video_link: string;
+    };
     rocket: {
-        rocket_name: string,
+        rocket_name: string;
         first_stage: {
-            cores: [
-                {
-                    flight: number,
-                    core: {
-                        reuse_count: number,
-                        status: string
-                    }
-                }
-            ]
-        },
-    },
-    second_stage: {
-        payloads: [
-            {
-                payload_type: string,
-                payload_mass_kg: number,
-                payload_mass_lbs: number
-            }
-        ]
-    }
+            cores: ICore[];
+        };
+    };
+    second_stage: IPayloads[];
 }
 
 
